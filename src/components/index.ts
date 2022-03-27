f4870ffec7a29243ef8113dc5c1bd37e3251b6cd
Nikhil Tayal <nikhil.tayal@dripcapital.com>
2022-03-27 23:14:13 +0530

    components exported

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
 export { default as AppButton } from "./AppButton";
 export { default as Card } from "./Card";
 export { default as Input } from "./Input";
+export { default as Box } from "./Box";
+export { default as Container } from "./Container";
+export { default as Grid } from "./Grid";
+export { default as Link } from "./Link";
+export { default as Typography } from "./Typography";
